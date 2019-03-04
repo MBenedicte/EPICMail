e8de58bc215342ac40128d889c20f5a5fca0b196
benedicte <benedictemusabimana@gmail.com>
2019-03-04 15:37:51 +0200

    add required frameworks

---
+++ SERVER/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('hello world /javascript');
+})
+
+const port=process.env.PORT||8000;
+app.listen(port, ()=>console.log(`The server is listening on port ${port}`));
