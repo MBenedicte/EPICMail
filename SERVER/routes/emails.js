f060d77770534afc4191a36529a53d46936c95ef
benedicte <benedictemusabimana@gmail.com>
2019-03-05 17:38:11 +0200

    Add a url to delete a specific email

--- SERVER/routes/emails.js
+++ SERVER/routes/emails.js
@@ -6,5 +6,6 @@ routers.get('/messages', Mails.allReceivedMail);
 routers.get('/messages/unread', Mails.unreadMail)
 routers.get('/messages/sent',Mails.sentMail)
 routers.get('/messages/:id',Mails.emailById);
+routers.delete('/messages/:id',Mails.deleteEmail);
 
 export default routers;
