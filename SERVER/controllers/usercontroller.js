c5e1c6437de8456c49dd88e5e825c5aee8c1678c
benedicte <benedictemusabimana@gmail.com>
2019-03-07 15:27:49 +0200

    Add configurations

---
+++ SERVER/controllers/usercontroller.js
@@ -0,0 +1,72 @@
+import users from '../models/users';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+
+export default class User{
+    static getAllUsers(req,res){
+        res.send(users);
+    }
+
+    static async userSignup(req,res){
+        let user= users.find(item=>item.username === req.body.username);
+        if(user) return res.send({
+            status: 400,
+            message:"Users already exists"
+        })
+
+        user={
+            id: users.length+1,
+            fistname: req.body.fistname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        user.password= await bcrypt.hash(user.password,salt);
+        
+        // const token= jwt.sign({id: user.id},'jwtPrivatekey')
+
+        const token= jwt.sign({id: user.id},config.get('jwtPrivatekey'))
+
+        users.push(user);
+
+        res.send({
+            status: 200,
+            data:[{
+                'token':token
+            }]
+        })
+    }
+    static async userLogin(req,res){
+        let user= users.find(item=>item.username === req.body.username);
+        if(!user) return res.send({
+            status: 400,
+            message:"Invalid username or password"
+        })
+
+        user={
+            username: req.body.username,
+            password: req.body.password
+        }
+
+        const validPassword= bcrypt.compare(req.body.password, user.password);
+        if(!validPassword) return res.send({
+            status: 400,
+            message:"Invalid username or password"
+        })
+
+        // const token= jwt.sign({id: user.id},'jwtPrivatekey')
+        const token= jwt.sign({id: user.id},config.get('jwtPrivatekey'))
+        res.send({
+            status: 200,
+            data:[{
+                'token':token
+            }]
+        })
+    }
+
+}
