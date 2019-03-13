76f4c5ad1b28f9c2b392d3a6beaa2b667261c1a5
benedicte <benedictemusabimana@gmail.com>
2019-03-13 14:53:45 +0200

    Chore: Add validation to user endpoints

--- SERVER/controllers/usercontroller.js
+++ SERVER/controllers/usercontroller.js
@@ -1,6 +1,7 @@
 import users from '../models/users';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import validateNewUser from '../Helper/validation'
 
 
 export default class User{
@@ -9,20 +10,31 @@ export default class User{
     }
 
     static async userSignup(req,res){
-        let user= users.find(item=>item.username === req.body.username);
-        if(user) return res.status(400).send({
-            status: 400,
-            message:"Users already exists"
-        });
 
-        user={
+        let usere= users.find(item => item.username === req.body.username);
+        if(usere)
+         {
+            return res.status(400).send({
+                status: 400,
+                message:"Users already exists"
+            });
+    
+         }
+        let user ={
             id: users.length+1,
-            fistname: req.body.fistname,
+            firstname: req.body.firstname,
             lastname: req.body.lastname,
             email: req.body.email,
             username: req.body.username,
             password: req.body.password
+        }       
+
+        const { error }= validateNewUser.validateReistration(req.body)
+        if( error ){
+            res.status(400).send(error.details[0].message)
+            return;
         }
+
 
         const salt = await bcrypt.genSalt(10);
         user.password= await bcrypt.hash(user.password,salt);
@@ -31,17 +43,25 @@ export default class User{
 
         users.push(user);
 
-        res.send({
+        res.status(200).send({
             status: 200,
             message:"User registered successfully",
             data:[{
                 'token':token
             }]
+            
         })
     }
     static async userLogin(req,res){
+
+        const { error }= validateNewUser.validateLogin(req.body)
+        if( error ){
+            res.status(400).send(error.details[0].message)
+           return;
+        }
+
         let user= users.find(item=>item.username === req.body.username);
-        if(!user) return res.send({
+        if(!user) return res.status(400).send({
             status: 400,
             message:"Invalid username or password"
         })
@@ -52,13 +72,13 @@ export default class User{
         }
 
         const validPassword= bcrypt.compare(req.body.password, user.password);
-        if(!validPassword) return res.send({
+        if(!validPassword) return res.status(400).send({
             status: 400,
             message:"Invalid username or password"
         })
 
         const token= jwt.sign({id: user.id},process.env.JWTPRIVATEKEY)
-        res.send({
+        res.status(200).send({
             status: 200,
             message: "User signed in successfully",
             data:[{
@@ -68,3 +88,4 @@ export default class User{
     }
 
 }
+
