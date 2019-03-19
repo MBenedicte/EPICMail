cf73e39bc9e30fda95cf8ca481a35e150e3b9739
benedicte <benedictemusabimana@gmail.com>
2019-03-20 01:35:59 +0200

    Chore: Create all tables

--- SERVER/database/index.js
+++ SERVER/database/index.js
@@ -1,9 +1,9 @@
-import { Client } from 'pg';
+import pg from 'pg';
 import dotenv from 'dotenv'
 
 dotenv.config();
 
-const client = new Client({
+const pool = new pg.Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -11,25 +11,12 @@ const client = new Client({
 
 });
 
-execute();
 
-async function execute(){
-  try{
-      await client.connect();
-      console.log(`connected successfully`);
-      const { rows } = await client.query(" select * from users");
-      console.log(rows)
-  }
-  catch(error){
-    console.log(`something wrong happened`)
-  }
-}
-
-// client.connect()
-
-// .then(()=> console.log('Connected successfully'))
-// .then(async()=> await client.query('select * from users'))
-// .then(result=> console.table(result.rows))
-// .catch(err=>console.log(`something went wrong here is the error ${err}`))
+pool.on('connect',()=>{
+  console.log(`connected successfully`);
+})
+      
+  
+  export default pool;
 
 
